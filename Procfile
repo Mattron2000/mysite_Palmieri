e6f7704253970a6ed74001962a630314cb27e8e4
Mattron2000 <65219141+Mattron2000@users.noreply.github.com>
2020-05-27 12:08:54 +0200

    add mods & Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mysite-Palmieri.wsgi --log-file - 
--- mysite_Palmieri/settings.py
+++ mysite_Palmieri/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3uz!k0w-0)#)+thv&h17%=c@dqy=vmcbu#rpfm(bw5-#!*5f-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite_Palmieri.urls'
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
